6ee402e688008d26ae853995fabc2925b08d5e66
Marcus McBride <marcusmcb@gmail.com>
2022-03-13 12:26:16 -0700

    added base layout and styling to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>!npBot for Serato Pro DJ & Twitch</p>
-      </header>
+    <div className='App'>
+      <div className='app-title'>
+        <p>!npBot for Serato Pro DJ & Twitch</p>        
+      </div>
+      <div className='row'>
+        <div className='column'>
+          <p>One</p>
+        </div>
+        <div className='column'>
+          <p>Two</p>
+        </div>
+      </div>
+      <div className='app-footer'>
+        <p>MCB Engineering, 2022</p>        
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
