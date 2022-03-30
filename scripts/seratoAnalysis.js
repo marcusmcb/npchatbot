7bf55226725c0162af6132314f0546edf53f890e
Marcus McBride <marcusmcb@gmail.com>
2022-03-30 16:27:01 -0700

    tested creds and comms on serato report script, working

--- scripts/seratoAnalysis.js
+++ scripts/seratoAnalysis.js
@@ -2,9 +2,11 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const dotenv = require('dotenv')
 
-dotenv.config()
+dotenv.config({ path: `../.env` })
 
-const url = `https://serato.com/playlists/${process.env.SERATO_DISPLAY_NAME}/live`
+const url = `https://serato.com/playlists/${process.env.SERATO_DISPLAY_NAME}/3-17-2022_1`
+// test url
+// const url = "https://serato.com/playlists/DJ_Marcus_McBride/3-17-2022_1"
 
 const createSeratoReport = async () => {
   try {
@@ -22,11 +24,13 @@ const createSeratoReport = async () => {
           let trackId = results[i].children[0].data.trim()
           tracksPlayed.push(trackId)
         }
+        console.log("* * * * * * * * * * * * * * * * *")
+        console.log(tracksPlayed)
+        console.log("* * * * * * * * * * * * * * * * *")
       })
       .catch((error) => {
         if (error.response.status === 404) {
-          process.stderr.write('Your Serato URL is incorrect.')
-          client.say(channel, "That doesn't appear to be working right now.")
+          process.stderr.write('Your Serato URL is incorrect.')          
         }
       })
   } catch (err) {
@@ -35,3 +39,4 @@ const createSeratoReport = async () => {
 }
 
 createSeratoReport()
+// call func to send email report w/stats & data
