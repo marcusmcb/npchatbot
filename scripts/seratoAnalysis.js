4c94879dd9a26126fc3cf9d791919e218d780959
Marcus McBride <marcusmcb@gmail.com>
2022-04-01 13:14:55 -0700

    working on time diff calcs and averages

--- scripts/seratoAnalysis.js
+++ scripts/seratoAnalysis.js
@@ -28,26 +28,41 @@ const createSeratoReport = async () => {
         // loop through track timestamps and clean data from scrape
         for (let j = 0; j < results.length; j++) {
           let timestamp = timestamps[j].children[0].data.trim()
+          timestamp = new Date("01/01/2020 " + timestamp )
           trackTimestamps.push(timestamp)
         }
+
+        console.log(trackTimestamps)
+
         // combine cleaned data into array of objects
-        let trackLog = tracksPlayed.map((result, index) => {
+        let trackLog = tracksPlayed.map((result, index) => {          
           return { trackId: result, timestamp: trackTimestamps[index]}
         })
         console.log("* * * * * * * * * * * * * * * * *")        
         console.log(trackLog)
-        console.log("* * * * * * * * * * * * * * * * *")     
+        console.log("* * * * * * * * * * * * * * * * *") 
         
-        let x = new Date("01/01/2020 " + trackLog[2].timestamp)
-        let y = new Date("01/01/2020 " + trackLog[3].timestamp)
-        console.log(x.getMinutes(), x.getSeconds())
-        console.log(y.getMinutes(), y.getSeconds())
-        console.log((y.getMinutes() - x.getMinutes()) + ":" + (y.getSeconds() - x.getSeconds()))        
+        // let x = trackLog[2].timestamp
+        // console.log(x.getTime())
+        // let y = trackLog[3].timestamp
+        // console.log(y.getTime())
+        
+        let timeDiffs = []
+
+        for (let k = 0; k < trackTimestamps.length; k++) {
+          let x = trackTimestamps[k + 1] - trackTimestamps[k]
+          timeDiffs.push(x)
+        }  
+        console.log(timeDiffs)
+        const avg = arr => arr.reduce((a,b) => a + b, 0) / arr.length      
+        console.log(avg(timeDiffs))
+
       })      
       .catch((error) => {        
-        if (error.response.status === 404) {
-          process.stderr.write('Your Serato URL is incorrect.')          
-        }
+        // if (error.response.status === 404) {
+        //   process.stderr.write('Your Serato URL is incorrect.')          
+        // }
+        console.log(error)
       })
   } catch (err) {
     console.log(err)
