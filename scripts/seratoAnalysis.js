75483e047d776117bdbf07fac7e40076d237888f
Marcus McBride <marcusmcb@gmail.com>
2022-04-01 19:38:50 -0700

    removed moment, added avg track length played, shortest/longest track, etc

--- scripts/seratoAnalysis.js
+++ scripts/seratoAnalysis.js
@@ -1,6 +1,5 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const moment = require('moment')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: `../.env` })
@@ -19,7 +18,7 @@ const createSeratoReport = async () => {
 
         let tracksPlayed = []
         let trackTimestamps = []
-        
+
         // loop through tracks played and clean data from scrape
         for (let i = 0; i < results.length; i++) {
           let trackId = results[i].children[0].data.trim()
@@ -28,40 +27,76 @@ const createSeratoReport = async () => {
         // loop through track timestamps and clean data from scrape
         for (let j = 0; j < results.length; j++) {
           let timestamp = timestamps[j].children[0].data.trim()
-          timestamp = new Date("01/01/2020 " + timestamp )
+          timestamp = new Date('01/01/2020 ' + timestamp)
           trackTimestamps.push(timestamp)
         }
 
-        console.log(trackTimestamps)
-
         // combine cleaned data into array of objects
-        let trackLog = tracksPlayed.map((result, index) => {          
-          return { trackId: result, timestamp: trackTimestamps[index]}
+        let trackLog = tracksPlayed.map((result, index) => {
+          return { trackId: result, timestamp: trackTimestamps[index] }
         })
-        console.log("* * * * * * * * * * * * * * * * *")        
+        console.log('* * * * * * * * * * * * * * * * *')
         console.log(trackLog)
-        console.log("* * * * * * * * * * * * * * * * *") 
+        console.log('* * * * * * * * * * * * * * * * *')
         
-        // let x = trackLog[2].timestamp
-        // console.log(x.getTime())
-        // let y = trackLog[3].timestamp
-        // console.log(y.getTime())
-        
+        console.log("Total tracks played: ", trackLog.length)
+        // determine lengths of each track played
         let timeDiffs = []
 
         for (let k = 0; k < trackTimestamps.length; k++) {
           let x = trackTimestamps[k + 1] - trackTimestamps[k]
-          timeDiffs.push(x)
-        }  
-        console.log(timeDiffs)
-        const avg = arr => arr.reduce((a,b) => a + b, 0) / arr.length      
-        console.log(avg(timeDiffs))
+          if (Number.isNaN(x)) {
+            
+          } else {
+            timeDiffs.push(x)
+          }
+        }
 
-      })      
-      .catch((error) => {        
-        // if (error.response.status === 404) {
-        //   process.stderr.write('Your Serato URL is incorrect.')          
-        // }
+        // longest track played
+        let max = Math.max(...timeDiffs)
+        let maxIndex = timeDiffs.indexOf(max)        
+        console.log("Longest track played: ", trackLog[maxIndex].trackId)
+        let longestTrack = Math.abs((trackTimestamps[maxIndex] - trackTimestamps[maxIndex + 1]) / 1000)        
+        let longestMinutes = Math.floor(longestTrack / 60) % 60
+        let longestSeconds = longestTrack % 60
+        if (longestSeconds < 10) {          
+          longestSeconds = "0" + longestSeconds
+          console.log(longestMinutes + ":" + longestSeconds)  
+        } else {          
+          console.log(longestMinutes + ":" + longestSeconds)  
+        }     
+
+        // shortest track played
+        let min = Math.min(...timeDiffs)
+        let minIndex = timeDiffs.indexOf(min)        
+        console.log("Shortest Track Played: ", trackLog[minIndex].trackId)
+        let shortestTrack = Math.abs((trackTimestamps[minIndex] - trackTimestamps[minIndex + 1]) / 1000)        
+        let shortestMinutes = Math.floor(shortestTrack / 60) % 60
+        let shortestSeconds = shortestTrack % 60
+        if (shortestSeconds < 10) {          
+          shortestSeconds = "0" + shortestSeconds
+          console.log(shortestMinutes + ":" + shortestSeconds)  
+        } else {          
+          console.log(shortestMinutes + ":" + shortestSeconds)  
+        }        
+        
+        // average track length played
+        let sumDiff = 0
+        for (let m = 0; m < timeDiffs.length; m++) {
+          sumDiff += timeDiffs[m]
+        }
+        let avg = sumDiff / timeDiffs.length
+        let w = (avg / 1000).toFixed()
+        let minutes = Math.floor(w / 60) % 60
+        let seconds = w % 60
+        if (seconds < 10) {          
+          seconds = "0" + seconds
+          console.log("Average Track Length Played: ", minutes + ":" + seconds)  
+        } else {          
+          console.log("Average Track Length Played: ", minutes + ":" + seconds)  
+        }        
+      })
+      .catch((error) => {
         console.log(error)
       })
   } catch (err) {
