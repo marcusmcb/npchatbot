166ecd53cf61a2fa432aa0800d90dcef7fcc351b
marcusmcb <marcusmcb@gmail.com>
2023-08-27 21:27:52 -0700

    add logic to display real time connection status in SessionPanel

--- client/src/components/SessionPanel.tsx
+++ client/src/components/SessionPanel.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 
 interface SessionPanelProps {
@@ -7,6 +8,45 @@ interface SessionPanelProps {
 }
 
 const SessionPanel: React.FC<SessionPanelProps> = (props) => {
+	const [hours, setHours] = useState(0)
+	const [minutes, setMinutes] = useState(0)
+	const [seconds, setSeconds] = useState(0)
+
+	const resetUptime = () => {
+		setHours(0)
+		setMinutes(0)
+		setSeconds(0)
+	}
+
+	useEffect(() => {
+		let interval: NodeJS.Timeout
+
+		if (props.isBotConnected) {
+			interval = setInterval(() => {
+				setSeconds((prevSeconds) => {
+					if (prevSeconds === 59) {
+						setMinutes((prevMinutes) => {
+							if (prevMinutes === 59) {
+								setHours((prevHours) => prevHours + 1)
+								return 0
+							}
+							return prevMinutes + 1
+						})
+						return 0
+					}
+					return prevSeconds + 1
+				})
+			}, 1000)
+		}
+
+		// Cleanup interval on unmount or when bot disconnects
+		return () => {
+			if (interval) {
+				clearInterval(interval)
+			}
+		}
+	}, [props.isBotConnected])
+
 	return (
 		<div className='app-container-column'>
 			<div className='app-form-title start-chatbot'>Chatbot Controls:</div>
@@ -21,17 +61,26 @@ const SessionPanel: React.FC<SessionPanelProps> = (props) => {
 				<button
 					className='bot-control-button'
 					type='submit'
-					onClick={props.handleDisconnect}
+					onClick={(event) => {
+						resetUptime();
+						props.handleDisconnect(event);
+				}}
 				>
 					End Session
 				</button>
 			</div>
 			<div className='app-form-title session-info'>Session Info:</div>
 			<div className='session-info-label'>
-				Status:<span className='session-info-status'>{props.isBotConnected ? 'connected' : 'not connected'}</span>
+				Status:
+				<span className='session-info-status'>
+					{props.isBotConnected ? 'connected' : 'not connected'}
+				</span>
 			</div>
 			<div className='session-info-label'>
-				Uptime:<span className='session-info-status'>x hours & x mins</span>
+				Uptime:
+				<span className='session-info-status'>
+					{hours > 0 ? `${hours} hours, ` : ''} {minutes > 0 ? `${minutes} mins, ` : ''} {seconds > 0 ? `${seconds} secs` : ''}
+				</span>
 			</div>
 		</div>
 	)
