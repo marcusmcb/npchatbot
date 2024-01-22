c071aeecef60a8efe243e96083e4e24b15d42808
marcusmcb <marcusmcb@gmail.com>
2024-01-21 20:40:35 -0800

    updated electron script to build client and run app

--- electron-main.js
+++ electron-main.js
@@ -1,34 +1,57 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+const { spawn } = require('child_process')
+let serverProcess
 
-let mainWindow;
+function startServer() {
+	// Start your Express server
+	serverProcess = spawn('npm', ['run', 'server'], {
+		cwd: process.cwd(),
+		detached: true,
+		stdio: 'inherit',
+	})
+
+	serverProcess.on('close', (code) => {
+		console.log(`Server process exited with code ${code}`)
+	})
+}
+
+let mainWindow
 
 app.on('ready', () => {
-  // ... Your code for starting the chatbot script ...
+	// ... Your code for starting the chatbot script ...
+	startServer()
+	// Create a new Electron browser window for the React app
+	mainWindow = new BrowserWindow({
+		width: 1280,
+		height: 680,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
 
-  // Create a new Electron browser window for the React app
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 680,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+	// Load the React app
+	const isDev = require('electron-is-dev')
 
-  // Load the React development server URL (not the production build URL)
-  mainWindow.loadURL('http://localhost:3000');
+	let appURL = isDev
+		? 'http://localhost:3000' // URL of your local dev server
+		: `file://${path.join(__dirname, '../client/build/index.html')}` // Path to your production build file
 
-  // Uncomment the following line if you want to open the DevTools for debugging
-  // mainWindow.webContents.openDevTools();
+	mainWindow.loadURL(appURL)
 
-  // ... Your code for handling window events ...
-});
+	// Uncomment the following line if you want to open the DevTools for debugging
+	// mainWindow.webContents.openDevTools();
+
+	// ... Your code for handling window events ...
+})
 
 // Handle the closing of the Electron app
 app.on('before-quit', () => {
-  // Close the Electron window when the app is closed
-  if (mainWindow) {
-    mainWindow.destroy();
-  }
-});
+	if (mainWindow) {
+		mainWindow.destroy()
+	}
+	if (serverProcess) {
+		process.kill(-serverProcess.pid) // Kills the entire subprocess tree
+	}
+})
