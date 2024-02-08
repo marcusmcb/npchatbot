168e75d16e6d97b8943661a5e327c30e9e6d2ce2
marcusmcb <marcusmcb@gmail.com>
2024-02-08 11:19:43 -0800

    fixed users nedb file location in database init method

--- database.js
+++ database.js
@@ -2,6 +2,6 @@ const Datastore = require('nedb');
 const path = require('path');
 
 const db = {};
-db.users = new Datastore({ filename: path.join(__dirname, 'users.db'), autoload: true });
+db.users = new Datastore({ filename: path.join(__dirname, './users.db'), autoload: true });
 
 module.exports = db;
