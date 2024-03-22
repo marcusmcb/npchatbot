439457cb9f16541adc1b065a2bf80e672ba81a60
marcusmcb <marcusmcb@gmail.com>
2024-03-22 11:46:55 -0700

    error handling on twitch channel name, params update helper method added

--- electron-main.js
+++ electron-main.js
@@ -78,6 +78,64 @@ const twitchURLValidityCheck = async (twitchDisplayName) => {
 	}
 }
 
+const updateUserData = async (db, event, arg) => {
+	db.users.findOne({}, async (err, existingUser) => {
+		if (err) {
+			console.error('Error fetching the user:', err)
+			event.reply('userDataResponse', { error: 'ipcMain: no user found' })
+		}
+
+		// If there's an existing user, update only the changed fields
+		if (existingUser) {
+			const updatedUser = { ...existingUser }
+
+			// Iterate over the keys in the request body to update only changed values
+			Object.keys(arg).forEach((key) => {
+				if (arg[key] !== existingUser[key]) {
+					updatedUser[key] = arg[key]
+				}
+			})
+
+			// Special handling for the twitchOAuthKey to use the encrypted version
+			// if (req.body.twitchOAuthKey) {
+			// 	updatedUser.twitchOAuthKey = encryptedOAuthKey
+			// }
+
+			try {
+				const numReplaced = await new Promise((resolve, reject) => {
+					db.users.update(
+						{ _id: existingUser._id },
+						{ $set: updatedUser },
+						{},
+						(err, numReplaced) => {
+							if (err) {
+								reject(err)
+							} else {
+								resolve(numReplaced)
+							}
+						}
+					)
+				})
+				console.log(`Updated ${numReplaced} user(s) with new data.`)
+				event.reply('userDataResponse', {
+					success: 'User data successfully updated',
+				})
+			} catch (error) {
+				console.error('Error updating the user:', error)
+				event.reply('userDataResponse', {
+					error: 'Error updating user data',
+				})
+			}
+		} else {
+			// If there's no existing user, insert a new one (or handle as an error)
+			console.log(
+				'No existing user found. Inserting new user or handling error.'
+			)
+			// Insert new user logic here, or return an error response
+		}
+	})
+}
+
 // Express routes
 server.get('/', (req, res) => {
 	res.send('NPChatbot is up and running')
@@ -266,65 +324,16 @@ ipcMain.on('submitUserData', async (event, arg) => {
 	const isValidTwitchURL = await twitchURLValidityCheck(arg.twitchChannelName)
 	if (isValidTwitchURL === true) {
 		console.log('Valid Twitch URL')
+		// call update params helper method here
 	} else {
 		console.log('Invalid Twitch URL')
+		event.reply('userDataResponse', {
+			error: 'The Twitch profile name given is invalid',
+		})
 	}
 	if (isValidSeratoURL === true) {
-		db.users.findOne({}, async (err, existingUser) => {
-			if (err) {
-				console.error('Error fetching the user:', err)
-				event.reply('userDataResponse', { error: 'ipcMain: no user found' })
-			}
-
-			// If there's an existing user, update only the changed fields
-			if (existingUser) {
-				const updatedUser = { ...existingUser }
-
-				// Iterate over the keys in the request body to update only changed values
-				Object.keys(arg).forEach((key) => {
-					if (arg[key] !== existingUser[key]) {
-						updatedUser[key] = arg[key]
-					}
-				})
-
-				// Special handling for the twitchOAuthKey to use the encrypted version
-				// if (req.body.twitchOAuthKey) {
-				// 	updatedUser.twitchOAuthKey = encryptedOAuthKey
-				// }
-
-				try {
-					const numReplaced = await new Promise((resolve, reject) => {
-						db.users.update(
-							{ _id: existingUser._id },
-							{ $set: updatedUser },
-							{},
-							(err, numReplaced) => {
-								if (err) {
-									reject(err)
-								} else {
-									resolve(numReplaced)
-								}
-							}
-						)
-					})
-					console.log(`Updated ${numReplaced} user(s) with new data.`)
-					event.reply('userDataResponse', {
-						success: 'User data successfully updated',
-					})
-				} catch (error) {
-					console.error('Error updating the user:', error)
-					event.reply('userDataResponse', {
-						error: 'Error updating user data',
-					})
-				}
-			} else {
-				// If there's no existing user, insert a new one (or handle as an error)
-				console.log(
-					'No existing user found. Inserting new user or handling error.'
-				)
-				// Insert new user logic here, or return an error response
-			}
-		})
+		await updateUserData(db, event, arg)
+		// move update params into callable helper method
 	} else {
 		event.reply('userDataResponse', {
 			error: 'The Serato profile name given is invalid',
