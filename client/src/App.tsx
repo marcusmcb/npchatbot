704f389435b48c7a00229c51a641253417d466eb
marcusmcb <marcusmcb@gmail.com>
2025-01-01 19:27:38 -0800

    updated playlist validation text response and styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -208,6 +208,8 @@ const App = (): JSX.Element => {
 		return pattern.test(email)
 	}
 
+	// method to validate that the user's Serato Live Playlist is public
+	// and can be accessed by npChatbot
 	const validateLivePlaylist = async (
 		event: React.MouseEvent<HTMLButtonElement>
 	) => {
@@ -221,9 +223,12 @@ const App = (): JSX.Element => {
 					'Your Serato Live Playlist is public and ready for use with npChatbot.'
 				)
 			} else if (response && !response.isValid) {
-				addMessageToQueue(
+				setError(
 					'Your current Serato Live Playlist cannot be reached. Please ensure your playlist is both live and public and try again.'
 				)
+				setTimeout(() => {
+					setError('')
+				}, 5000)
 			} else if (response && response.error) {
 				console.error(response.error)
 				addMessageToQueue(response.error)
