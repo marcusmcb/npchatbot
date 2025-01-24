8791a6b3d6ede31fe3b8dcfd1c1977d0c4f273fa
marcusmcb <marcusmcb@gmail.com>
2025-01-23 19:55:32 -0800

    updated links in tool tip texts

--- client/src/components/tooltips/tooltipTexts.js
+++ client/src/components/tooltips/tooltipTexts.js
@@ -1,12 +1,18 @@
 const tooltipTexts = {
-  twitchChannelName: "Enter your primary Twitch© channel name here.",
-  twitchChatbotName: "Enter your Twitch© channel's chatbot name here. More details can be found <a href='https://www.npchatbot.com/' rel='noreferrer' target='_blank'>here</a>.",
-  obsClearDisplayTime: "Enter the duration (in seconds) for the OBS response to remain on screen.   This feature is enabled when you enter your OBS Websocket address.",
-  seratoDisplayName: "Enter your Serato© profile display name here.",
-  intervalMessageDuration: "Enter the duration (in minutes) for automatic interval messages to appear.",
-  obsWebsocketAddress: "Enter your local OBS web socket address here (optional).",
-  obsWebsocketPassword: "If your web socket connection is secured within OBS, please enter the password here (optional)",
-  userEmailAddress: "Enter the email address that you'd like your post-stream report sent to",
-};
+	twitchChannelName: 'Enter your primary Twitch© channel name here.',
+	twitchChatbotName:
+		"Enter your Twitch© channel's chatbot name here. More details can be found <a href='https://www.npchatbot.com/' rel='noreferrer' target='_blank'>here</a>.",
+	obsClearDisplayTime:
+		'Enter the duration (in seconds) for the OBS response to remain on screen.   This feature is enabled when you enter your OBS Websocket address.',
+	seratoDisplayName: 'Enter your Serato© profile display name here.  More details on how to find this can found <a href="https://www.npchatbot.com/installation?tab=Credentials" rel="noreferrer" target="_blank">here</a>.', 
+	intervalMessageDuration:
+		"Enter the duration (in minutes) for automatic interval messages to appear.  More details about this feature can be found <a href='https://www.npchatbot.com/installation?tab=Preferences' rel='noreferrer' target='_blank'>here</a>.",
+	obsWebsocketAddress:
+		'Enter your local OBS web socket address here (optional).',
+	obsWebsocketPassword:
+		'If your web socket connection is secured within OBS, please enter the password here (optional)',
+	userEmailAddress:
+		"Enter the email address that you'd like your post-stream report sent to",
+}
 
-export default tooltipTexts;
+export default tooltipTexts
