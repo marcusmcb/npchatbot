5d0324b10e7e41757a3e08005a3d40e28c972381
marcusmcb <marcusmcb@gmail.com>
2025-01-29 11:04:47 -0800

    moved helper methods, added logging to spotify API results

---
+++ bot-assets/post-stream-report/helpers/spotifyHelpers.js
@@ -0,0 +1,33 @@
+const getCurrentDate = () => {
+	const date = new Date()
+	const options = { weekday: 'long', month: 'long', day: 'numeric' }
+
+	// format the date using Intl.DateTimeFormat
+	let formattedDate = new Intl.DateTimeFormat('en-US', options).format(date)
+
+	// add the appropriate suffix to the day (e.g., "st", "nd", "rd", "th")
+	const day = date.getDate()
+	const suffix =
+		day % 10 === 1 && day !== 11
+			? 'st'
+			: day % 10 === 2 && day !== 12
+			? 'nd'
+			: day % 10 === 3 && day !== 13
+			? 'rd'
+			: 'th'
+
+	// replace the numeric day in the formatted string with the day + suffix
+	formattedDate = formattedDate.replace(/\d+/, `${day}${suffix}`)
+	return formattedDate
+}
+
+const cleanSongTitle = (title) => {
+	return title
+		.replace(/\s*[\(\[].*?[\)\]]/g, '')
+		.replace(/[&.,-]/g, '')
+		.replace(/\s+/g, ' ')
+		.trim()
+		.toLowerCase()
+}
+
+module.exports = { getCurrentDate, cleanSongTitle } 
