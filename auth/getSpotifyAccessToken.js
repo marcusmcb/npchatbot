d6f592da330f2447921ed2042f813e9513f06d6b
marcusmcb <marcusmcb@gmail.com>
2025-01-29 10:21:43 -0800

    moved spotify access token flow to auth, updated spotify text strings

---
+++ auth/getSpotifyAccessToken.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN
+const clientId = process.env.SPOTIFY_CLIENT_ID
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
+
+const getSpotifyAccessToken = async () => {
+	try {
+		const response = await axios.post(
+			'https://accounts.spotify.com/api/token',
+			null,
+			{
+				params: {
+					grant_type: 'refresh_token',
+					refresh_token: refreshToken,
+					client_id: clientId,
+					client_secret: clientSecret,
+				},
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			}
+		)		
+		return response.data.access_token
+	} catch (error) {
+		console.error('Error refreshing access token:', error.response.data)
+	}
+}
+
+module.exports = { getSpotifyAccessToken }
