785f6b2a10a6c30b0a6e245f557ae21b0966c059
marcusmcb <marcusmcb@gmail.com>
2025-02-05 20:11:43 -0800

    added spotify toggle in prefs component and tooltip logic

--- client/src/components/tooltips/tooltipTexts.js
+++ client/src/components/tooltips/tooltipTexts.js
@@ -4,7 +4,8 @@ const tooltipTexts = {
 		"Enter your Twitch© channel's chatbot name here. More details can be found <a href='https://www.npchatbot.com/' rel='noreferrer' target='_blank'>here</a>.",
 	obsClearDisplayTime:
 		'Enter the duration (in seconds) for the OBS response to remain on screen.   This feature is enabled when you enter your OBS Websocket address.',
-	seratoDisplayName: 'Enter your Serato© profile display name here.  More details on how to find this can found <a href="https://www.npchatbot.com/installation?tab=Credentials" rel="noreferrer" target="_blank">here</a>.', 
+	seratoDisplayName:
+		'Enter your Serato© profile display name here.  More details on how to find this can found <a href="https://www.npchatbot.com/installation?tab=Credentials" rel="noreferrer" target="_blank">here</a>.',
 	intervalMessageDuration:
 		"Enter the duration (in minutes) for automatic interval messages to appear.  More details about this feature can be found <a href='https://www.npchatbot.com/installation?tab=Preferences' rel='noreferrer' target='_blank'>here</a>.",
 	obsWebsocketAddress:
@@ -13,6 +14,8 @@ const tooltipTexts = {
 		'If your web socket connection is secured within OBS, please enter the password here (optional)',
 	userEmailAddress:
 		"Enter the email address that you'd like your post-stream report sent to",
+	spotifyPlaylistEnabled:
+		"With this feature enabled, a Spotify Playlist of your current set will be created when you connect npChatbot to Twitch.",
 }
 
 export default tooltipTexts
