30bd17df471ba04313c18c9dbd8e219e1d9a9172
marcusmcb <marcusmcb@gmail.com>
2025-02-14 13:32:10 -0800

    refactored auto-id helper methods

---
+++ bot-assets/auto-id/helpers/autoIdHelpers.js
@@ -0,0 +1,33 @@
+const scrapeData = require('../../commands/liveReport/LiveReportHelpers/scrapeData')
+
+const getUniqueSongs = (songArray) => {
+	return [...new Set(songArray)]
+}
+
+const hasSongBeenPlayed = (query, songArray) => {
+	return songArray.includes(query)
+}
+
+const checkCurrentSong = async (url) => {
+  try {
+    const response = await scrapeData(url)
+    const results = response[0]
+    if (results.length > 0) {
+      console.log('Current Song Playing: ')
+      console.log(results[0].children[0].data.trim())
+      console.log('--------------------')
+      return results[0].children[0].data.trim()
+    } else {
+      return null
+    }
+  } catch (error) {
+    console.log('Error checking current song playing: ', error)
+    return null
+  }
+}
+
+module.exports = {
+  getUniqueSongs,
+  hasSongBeenPlayed,
+  checkCurrentSong,
+}
