34d9c5b13ea7728e4b77994766c06410252163ec
marcusmcb <marcusmcb@gmail.com>
2025-02-15 17:02:33 -0800

    added vibecheck and doubles interval messages

--- bot-assets/auto-commands-list/autoCommandList.js
+++ bot-assets/auto-commands-list/autoCommandList.js
@@ -1,3 +1,16 @@
-const autoCommandList = ['npInfo', 'dypInfo', 'statsInfo']
+/*
+
+Rename this directory as "interval-messages-list" and update the necessary
+require statements throughout the project.
+
+*/
+
+const autoCommandList = [
+	'npInfo',
+	'dypInfo',
+	'statsInfo',
+	'doublesInfo',
+	'vibecheckInfo',
+]
 
 module.exports = [autoCommandList]
