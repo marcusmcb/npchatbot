cfe2513da148e8dde0c0051e4418a05193f6b27b
marcusmcb <marcusmcb@gmail.com>
2025-03-02 12:37:26 -0800

    removed logging, tested continue playlist feature, resuming properly

--- bot-assets/auto-id/trackCurrentSongPlaying.js
+++ bot-assets/auto-id/trackCurrentSongPlaying.js
@@ -70,15 +70,15 @@ const resumeSpotifyPlaylist = async (
 
 	/* LOGGING FOR TESTING */
 
-	console.log("Serato Playlist Data: ")
-	console.log("-------------------------------")
-	for (let i = 0; i < results.length; i++) {
-		console.log(results[i].children[0].data.trim())
-	}
-	console.log("-------------------------------")
+	// console.log("Serato Playlist Data: ")
+	// console.log("-------------------------------")
+	// for (let i = 0; i < results.length; i++) {
+	// 	console.log(results[i].children[0].data.trim())
+	// }
+	// console.log("-------------------------------")
 
-	console.log("Serato Playlist Length: ", seratoPlaylistLength)
-	console.log("Spotify Playlist Length: ", spotifyPlaylistLength)	
+	// console.log("Serato Playlist Length: ", seratoPlaylistLength)
+	// console.log("Spotify Playlist Length: ", spotifyPlaylistLength)	
 
 	if (spotifyPlaylistLength === seratoPlaylistLength) {
 		console.log('Spotify playlist is up to date with Serato playlist.')
