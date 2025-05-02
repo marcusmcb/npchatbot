90266cb7bde3090332a8fcdc4b6f02615839395c
marcusmcb <marcusmcb@gmail.com>
2025-05-02 10:41:26 -0700

    updated tool tip texts

--- client/src/components/tooltips/tooltipTexts.js
+++ client/src/components/tooltips/tooltipTexts.js
@@ -1,15 +1,15 @@
 const tooltipTexts = {
-	twitchChannelName: 'Enter your primary Twitch© channel name here.',
+	twitchChannelName: 'Enter your main Twitch© channel name here.',
 	twitchChatbotName:
-		"Enter your Twitch© channel's chatbot name here. More details can be found <a href='https://www.npchatbot.com/' rel='noreferrer' target='_blank'>here</a>.",
+		"Enter the name of the Twitch© channel you've linked npChatbot with here. More details can be found <a href='https://www.npchatbot.com/' rel='noreferrer' target='_blank'>here</a>.",
 	obsClearDisplayTime:
-		'Enter the duration (in seconds) for the OBS response to remain on screen.   This feature is enabled when you enter your OBS Websocket address.',
+		'Enter the duration (in seconds) for the OBS text response to remain on screen.   This feature is enabled when you enter your OBS Websocket address.',
 	seratoDisplayName:
-		'Enter your Serato© profile display name here.  More details on how to find this can found <a href="https://www.npchatbot.com/installation?tab=Credentials" rel="noreferrer" target="_blank">here</a>.',
+		'Enter your Serato© profile display name here.  More details on how to find this can be found <a href="https://www.npchatbot.com/installation?tab=Credentials" rel="noreferrer" target="_blank">here</a>.',
 	intervalMessageDuration:
-		"Enter the duration (in minutes) for automatic interval messages to appear.  More details about this feature can be found <a href='https://www.npchatbot.com/installation?tab=Preferences' rel='noreferrer' target='_blank'>here</a>.",
+		"Enter the interval (in minutes) for automatic chat messages to appear.  More details about this feature can be found <a href='https://www.npchatbot.com/installation?tab=Preferences' rel='noreferrer' target='_blank'>here</a>.",
 	obsWebsocketAddress:
-		'Enter your local OBS web socket address here (optional) in the same format shown in the example.',
+		'Enter your local OBS web socket address here (optional) in the same format shown in the example (address:port).',
 	obsWebsocketPassword:
 		'If your web socket connection is secured within OBS, please enter the password here (optional)',
 	userEmailAddress:
